4b860d86431f8397a7dcc3495d5f312d17069ce7
tama.asrory.ridhana@students.uin-suska.ac.id <tama.asrory.ridhana@students.uin-suska.ac.id>
2016-11-15 08:59:56 +0700

    jaringan syaraf tiruan - Perceptron - FIX -

--- ipwj/src/GrayScale.java
+++ ipwj/src/GrayScale.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author asrory
+ * @author Tama Asrory Ridhana
+ * @serial Pengolahan Citra Digital , Gary-Scale
+ * @version 1.0
+ * @since  هِجْرَة : ٩ سؤر ١٤٣٨  
+ * 
  */
 import java.awt.*;
 import java.awt.image.BufferedImage;
-
 import java.io.*;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,30 +20,50 @@ public class GrayScale extends JPanel {
     BufferedImage image;
     int width;
     int height;
+    int counter = 0;
 
     public ImageIcon gray() throws IOException {
-
-	File input = new File("/opt/lampp/htdocs/myrepo/java/image-processing-with-java/ipwj/src/tama.jpg");
+	File input = new File("/opt/lampp/htdocs/myrepo/java/image-processing-with-java/ipwj/src/tama1.png");
 	image = ImageIO.read(input);
 	width = image.getWidth();
 	height = image.getHeight();
-	System.out.println("W : "+width+" x"+" H : "+height);
-	
+//	System.out.println("W : " + width + " x" + " H : " + height); // print resolution
+	setColor(setPixel());
+	ImageIcon ic = new ImageIcon(image);
+	return ic;
+    }
+
+    public int[][] setPixel() {
+
+	int pixel[][] = new int[height][width];
+
 	for (int i = 0; i < height; i++) {
-
 	    for (int j = 0; j < width; j++) {
+		Color c = new Color(image.getRGB(j, i));
+		int red = (int) (c.getRed() & 0x000000FF0);
+		int green = (int) (c.getGreen() & 0x0000FF00) >> 8;
+		int blue = (int) (c.getBlue() & 0x00FF0000) >> 16;
+		counter += pixel[i][j] = (red + green + blue) / 3;
+	    }
+	}
+	return pixel;
+    }
 
-		Color c = new Color(image.getRGB(j, i));		
-		int red = (int) (c.getRed() * 0.299);
-		int green = (int) (c.getGreen() * 0.587);
-		int blue = (int) (c.getBlue() * 0.114);
-		int rgbTogray=(red + green + blue)/3;
-		Color gray = new Color(rgbTogray,rgbTogray,rgbTogray);
+    public void setColor(int[][] pixel) {
+//	System.out.println("RATA2 = " + (counter / (width * height))); // print average color in integer
+	for (int i = 0; i < height; i++) {
+	    for (int j = 0; j < width; j++) {
+		if (pixel[i][j] < (counter / (width * height))) {
+		    pixel[i][j] = 0;
+		} else {
+		    pixel[i][j] = 255;
+		}
+//		System.out.println("BINER = " + pixel[i][j]); // print BINARI color as LOG
+		Color gray = new Color(pixel[i][j], pixel[i][j], pixel[i][j]);
 		image.setRGB(j, i, gray.getRGB());
 	    }
 	}
-	ImageIcon ic = new ImageIcon(image);
-	return ic;
+
     }
 
     public static void main(String args[]) throws Exception {
@@ -58,7 +73,7 @@ public class GrayScale extends JPanel {
 	GrayScale obj = new GrayScale();
 	img.setIcon(obj.gray());
 	frame.add(img);
-	frame.setSize(obj.width,obj.height);
+	frame.setSize(obj.width, obj.height);
 	frame.setLocationRelativeTo(null);
 	frame.setVisible(true);
     }
